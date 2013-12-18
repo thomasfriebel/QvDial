4f53dbdccfe145e7fe1dc92a60ca9b162f7518b0
Thomas Friebel <thomasfriebel@gmail.com>
2013-12-18 13:34:25 +0100

    new step-width option
    added a new option step-width and upgraded to the latest version of
    jquery.knob.js

--- Script.js
+++ Script.js
@@ -1,34 +1,72 @@
 
 Qva.LoadScript(Qva.Remote + (Qva.Remote.indexOf('?') >= 0 ? '&' : '?') + 'public=only' + '&name=' + 'Extensions/QvDial/lib/js/jquery.knob.js', init_QvDial);
-
+if (Qva.Mgr.mySelect == undefined) {
+    Qva.Mgr.mySelect = function (owner, elem, name, prefix) {
+        if (!Qva.MgrSplit(this, name, prefix)) return;
+        owner.AddManager(this);
+        this.Element = elem;
+        this.ByValue = true;
+ 
+        elem.binderid = owner.binderid;
+        elem.Name = this.Name;
+ 
+        elem.onchange = Qva.Mgr.mySelect.OnChange;
+        elem.onclick = Qva.CancelBubble;
+    }
+    Qva.Mgr.mySelect.OnChange = function () {
+        var binder = Qva.GetBinder(this.binderid);
+        if (!binder.Enabled) return;
+        if (this.selectedIndex < 0) return;
+        var opt = this.options[this.selectedIndex];
+        binder.Set(this.Name, 'text', opt.value, true);
+    }
+    Qva.Mgr.mySelect.prototype.Paint = function (mode, node) {
+        this.Touched = true;
+        var element = this.Element;
+        var currentValue = node.getAttribute("value");
+        if (currentValue == null) currentValue = "";
+        var optlen = element.options.length;
+        element.disabled = mode != 'e';
+        //element.value = currentValue;
+        for (var ix = 0; ix < optlen; ++ix) {
+            if (element.options[ix].value === currentValue) {
+                element.selectedIndex = ix;
+            }
+        }
+        element.style.display = Qva.MgrGetDisplayFromMode(this, mode);
+ 
+    }
+}
 function init_QvDial() {
 	Qva.AddExtension('QvDial', function() {
 		var _this = this
-		_this.version = '1.0'
+		_this.version = '1.1'
+		_this._inputType = "text"
 		_this._uniqueId = _this.Layout.ObjectId.replace("\\","_");
 		_this._variable = getQVStringProp(0) //Variable name
 		_this._minValue = getQVStringProp(1) ? getQVStringProp(1) : 0 //0.3 - 0.8
 		_this._maxValue = getQVStringProp(2) ? getQVStringProp(2) : 100 //0.3 - 0.8
-		_this._fgColor = getQVStringProp(3) //fore ground color
-		_this._cursor = getQVStringProp(4) == 0 ? false : 12 //cursor mode (=1), else (=0)
-		_this._bgColor = getQVStringProp(5) //back ground color
+		_this._step = getQVStringProp(3) ? getQVStringProp(3) : 1 //0.3 - 0.8
+		_this._fgColor = getQVStringProp(4) // foreground color
+		_this._cursor = getQVStringProp(5) == 0 ? false : 12 //cursor mode (=1), else (=0)
+		_this._bgColor = getQVStringProp(6) //background color
 		_this._thickness = 0.4
 		_this._type = 'ring'
 		_this._offset = 0
-		if (getQVStringProp(6)=='thick'){ //normal,thin,thick
+		if (getQVStringProp(7)=='thick'){ //normal,thin,thick
 			_this._thickness = 0.5
 		}
-		else if (getQVStringProp(6)=='thin') {
+		else if (getQVStringProp(7)=='thin') {
 			_this._thickness = 0.3
 		}
-		if (getQVStringProp(7) == 'arc') {
+		if (getQVStringProp(8) == 'arc') {
 			_this._type = 'arc' 
 		}
-		if (getQVStringProp(8) == 'e')      {
+		if (getQVStringProp(9) == 'e')      {
 			_this._offset = 90
-		} else if (getQVStringProp(8) == 's') {
+		} else if (getQVStringProp(9) == 's') {
 			_this._offset = 180
-		} else if (getQVStringProp(8) == 'w') {
+		} else if (getQVStringProp(9) == 'w') {
 			_this._offset = 270
 		}
 				
@@ -60,7 +98,7 @@ function init_QvDial() {
 			$(_this._input).unbind('change')
 			$(_this.Element).empty()
 			
-			_this._input = $('<input>').attr({'id':'qvdial_'+_this._uniqueId,'type':'text'}).addClass("dial").val(_this._value).attr('title',_this._variable)
+			_this._input = $('<input>').attr({'id':'qvdial_'+_this._uniqueId,'type':_this._inputType}).addClass("dial").val(_this._value).attr('title',_this._variable)
 			$(_this.Element).append(_this._input)
 			//hook on standard change event
 			_this._input.change(function(ev){_this._onChange($(ev.target).val())})
@@ -79,12 +117,13 @@ function init_QvDial() {
 				,width: maxWidthHeight -5
 				,height: maxWidthHeight -5
 				,stopper:true
+				,step:_this._step
 				,'release' : function(v){_this._onChange(v)}
 			});
 		}
 		
 		_this._onChange = function(value) {
-			SetVarValue(_this._variable,value)
+		SetVarValue(_this._variable,value)
 		}
 		
 		_this.getVariable(_this._variable, function(){_this._paintInitial()})
